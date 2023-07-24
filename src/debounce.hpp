0e503558b68b0246479056c6c221f502a1d8cc35
Ed Cragg <ed.cragg@eipi.xyz>
2023-07-24 13:19:07 +0100

    Add debounce code

--- src/debounce.hpp
+++ src/debounce.hpp
@@ -14,11 +14,9 @@ struct Debounce {
     onDessertFn = fn;
   }
 
-  void deepDebounce() {
-    if (onAssertFn != NULL) onAssertFn();
-
-
-    if (onDessertFn != NULL) onDessertFn();
+  void dispatch() {
+    if (onAssertFn != NULL && asserted()) onAssertFn();
+    else if (onDessertFn != NULL && deasserted()) onDessertFn();
   }
 
   void enable() {
@@ -30,8 +28,11 @@ struct Debounce {
   }
 
   void tick() {
-    if (enabled) {
-
+    uint32_t time = millis();
+    if (enabled && time - lastCheck > interval) {
+      advance(digitalRead(pin));
+      dispatch();
+      lastCheck = time;
     }
   }
 
@@ -40,12 +41,36 @@ struct Debounce {
   }
 
 private:
+  uint8_t asserted() {
+    uint8_t asserted = 0;
+    if ((history & MASK) == 0b00000111) {
+      asserted = 1;
+      history = 0xFF;
+    }
+    return asserted;
+  }
+
+  uint8_t deasserted(){
+    uint8_t deasserted = 0;
+    if ((history & MASK) == 0b11000000) {
+      deasserted = 1;
+      history = 0x00;
+    }
+    return deasserted;
+  }
+
+  void advance(bool state) {
+    history <<= 1;
+    history |= state;
+  }
+
   bool enabled;
   uint8_t pin;
   uint8_t interval;
   uint32_t lastCheck;
   void (*onAssertFn)(void);
   void (*onDessertFn)(void);
+  uint8_t history = 0;
 };
 
 #endif // __DEBOUNCE_H__
